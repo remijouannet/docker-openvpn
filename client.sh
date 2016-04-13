236ab3f300965e3d61a1de9ab9017ac8666326be
remijouannet <remijouannet@gmail.com>
2016-04-13 23:51:13 +0000

    client server key, client.sh

---
+++ client.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+[ -f client.ovpn ] || cat >client.ovpn <<EOF
+remote $1 1294 udp
+client
+nobind
+dev tun
+auth-user-pass
+
+<key>
+`cat keys/client.key`
+</key>
+<cert>
+`cat keys/client.crt`
+</cert>
+<ca>
+`cat keys/ca.crt`
+</ca>
+<dh>
+`cat keys/dh.pem`
+</dh>
+EOF
